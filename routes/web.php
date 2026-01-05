27a3bb45082dfbde75212ac659d6afc615c36785
fidelcom <okpara2040@gmail.com>
2026-01-05 17:45:16 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\LandingController;
+use Illuminate\Support\Facades\Route;
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', [LandingController::class, 'index'])->name('home');
